79abde877629a943fa9756d5ef9c494e571fc06f
SaraKehtari <sara80.kehtari@gmail.com>
2023-01-20 13:11:19 +0330

    feat: add web sidebar and web panel

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Register from "./Pages/Register";
 import Panel from "./Pages/Panel";
 import Dhcpconfig from "./Pages/Panel/Dhcp-config";
 import Iprange from "./Pages/Panel/IPrange";
+import Webconfig from "./Pages/Panel/Web-config";
 
 export const routes = [
   // { path: "/", element: <Homepage /> },
@@ -16,6 +17,7 @@ export const routes = [
       // { path: "/dhcp-panel/log", element: <Favorites /> },
       { path: "dhcp-config", element: <Dhcpconfig /> },
       { path: "dhcp-ip", element: <Iprange /> },
+      { path: "web-config", element: <Webconfig /> },
     ],
   },
 ];

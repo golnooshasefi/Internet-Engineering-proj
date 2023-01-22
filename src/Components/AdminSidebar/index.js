8945b25cdcd6f24a036219a6477fab0f0f6287f7
SaraKehtari <sara80.kehtari@gmail.com>
2023-01-23 01:41:48 +0330

    feat: add admin sidebar

--- src/Components/AdminSidebar/index.js
+++ src/Components/AdminSidebar/index.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 
 import Toolbar from "@mui/material/Toolbar";
@@ -9,14 +8,24 @@ import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
+
 import MailIcon from "@mui/icons-material/Mail";
+import DnsIcon from "@mui/icons-material/Dns";
+import SettingsIcon from "@mui/icons-material/Settings";
+import BuildIcon from "@mui/icons-material/Build";
+import { Avatar, Typography } from "@mui/material";
+import { teal } from "@mui/material/colors";
 
 import classes from "../Shared/Sidebar.module.scss";
+import { UserContext } from "../../store/UserContext";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 const drawerWidth = 260;
 function Adminsidebar() {
+  const context = useContext(UserContext);
+  const { user } = context;
+
   return (
     <Drawer
       sx={{
@@ -30,34 +39,99 @@ function Adminsidebar() {
       variant="permanent"
       anchor="left"
     >
-      <Toolbar />
+      <Toolbar className={classes.tool}>
+        <Typography variant="h6" noWrap component="div">
+          User panel
+        </Typography>
+      </Toolbar>
+      <Divider />
+      <div className={classes["user-info"]}>
+        <div className={classes["user-info__description"]}>
+          <div className={classes.info}>
+            <Avatar
+              sx={{ bgcolor: teal[500] }}
+              className={classes.container__form_Avatar}
+            ></Avatar>
+            {/* <div className={classes["user-info__title"]}>{user.username}</div> */}
+            <div className={classes["user-info__title"]}>{user.username}</div>
+          </div>
+        </div>
+      </div>
+
+      <Divider />
+
       <Divider />
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
+        {/* {["All mail", "Trash", "Spam"].map((text, index) => ( */}
+        <Link to="logs" className={classes.link}>
+          <ListItem disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                <DnsIcon />
               </ListItemIcon>
-              <ListItemText primary={text} />
+              Recieve your log
             </ListItemButton>
           </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
+        </Link>
+
+        <Link to="dhcp-config" className={classes.link}>
+          <ListItem disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                <SettingsIcon />
               </ListItemIcon>
-              <ListItemText primary={text} />
+              DHCP Config
             </ListItemButton>
           </ListItem>
-        ))}
+        </Link>
+
+        <Link to="dhcp-ip" className={classes.link}>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <BuildIcon />
+              </ListItemIcon>
+              Change Ip Range
+            </ListItemButton>
+          </ListItem>
+        </Link>
+
+        <Link to="web-config" className={classes.link}>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              Web Server Config
+            </ListItemButton>
+          </ListItem>
+        </Link>
+
+        <Link to="web-change-dir" className={classes.link}>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <BuildIcon sx={{ fontSize: 20 }} />
+              </ListItemIcon>
+              Change Home Directory
+            </ListItemButton>
+          </ListItem>
+        </Link>
+
+        <Link to="mail-config" className={classes.link}>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <MailIcon />
+              </ListItemIcon>
+              Mail Config
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        {/* ))} */}
       </List>
     </Drawer>
+    // </Box>
   );
 }
 
